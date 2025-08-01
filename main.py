deeea3bdb21c95b9f46410ec2b52c2dffb05a1e6
odiegosilva1 <odiegosilvadev@gmail.com>
2025-08-01 09:39:20 -0300

    add: columns

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import create_engine # type: ignore
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, ForeignKey # type: ignore
 from sqlalchemy.orm import sessionmaker, declarative_base # type: ignore
 
 db = create_engine('sqlite:///meubanco.db')
@@ -7,6 +7,20 @@ Session = sessionmaker(bind=db)
 session = Session()
 
 
+Base = declarative_base
+
+
+#add tables users
+class Users(Base):
+  id = Column("id", Integer)
+  name = Column("name", String)
+  email = Column("email", String)
+  password = Column("password", String)
+  active = Column("active", Boolean)
+
+# add tables products
+class Products(Base):
+  id = Column("id", Integer)
 
 Base = declarative_base()
 Base.metadata.create_all(bind=db)
